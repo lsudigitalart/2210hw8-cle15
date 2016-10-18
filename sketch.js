51a500c5824d31e449071d5552313d11c9820592
Cuong Le <cle15@lsu.edu>
2016-10-18 09:29:15 -0500

    not complete

--- sketch.js
+++ sketch.js
@@ -3,10 +3,10 @@ function setup(){
 }
 
 function draw(){
-charGen(2,3,6);
+charGen(2,3,6,3);
 }
 function mouseReleased(){
-  background(0);
+  background(255);
   charGen(mouseX, mouseY, 100);
 }
 function charGen(eyes,mouth,body,arms){
@@ -23,8 +23,8 @@ fill(random(255), random(255), random(255));
 ellipse(width/2, centerMass - 15 * random(2), map(eyes, mouth, mouth, 20, 80), 10);
 
 fill(random(255), random(255), random(255));
-line(arms, centerMass, 5,5);
-line(arms+5, centerMass, 5,5);
+//line(centerMass, centerMass, 5,5);
+//line(centerMass, centerMass, 5,5);
 
 
 }
