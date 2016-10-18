9f3b02bcd059fda37915225dfb98947d919ac6a7
Cuong Le <cle15@lsu.edu>
2016-10-18 04:59:29 -0500

    rip

--- sketch.js
+++ sketch.js
@@ -3,21 +3,27 @@ function setup(){
 }
 
 function draw(){
-
+charGen(2,3,6);
 }
 function mouseReleased(){
   background(0);
   charGen(mouseX, mouseY, 100);
 }
-function charGen(eyes,mouth,body){
+function charGen(eyes,mouth,body,arms){
 centerMass = height/2;
 fill(random(255), random(255), random(255));
 ellipse(width/2, centerMass, body, body *random(3));
 
-fill(random(255, random(255), random(255)));
+fill(random(255), random(255), random(255));
 rectMode(CENTER);
 rect(width/2, centerMass - 20 *random(2), map(eyes, 0 , width, 10, 40), 8);
 
-fill(random(255, random(255), random(255)));
+fill(random(255), random(255), random(255));
 ellipse(width/2, centerMass - 15 * random(2), map(eyes, mouth, mouth, 20, 80), 10);
+
+fill(random(255), random(255), random(255));
+line(arms, centerMass, 5,5);
+line(arms+5, centerMass, 5,5);
+
+
 }
